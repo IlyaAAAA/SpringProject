3cb4fae79d79e026dfce9669e1478a2788364c7f
IlyaAAAA <ilya-sachuk@mail.ru>
2022-11-29 20:06:28 +0300

    added service

---
+++ src/main/java/main/service/CalcService.java
@@ -0,0 +1,12 @@
+package main.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalcService {
+
+    public int sumInts(int x, int y) {
+        return x + y;
+    }
+
+}
